d489de49e4c4adb8cc2dcda57154d7fc0da7a186
Daniel Nogueira <silvanogueira3460@gmail.com>
2022-06-29 15:30:20 -0300

    Subindo arquivos para o repositorio

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (request, response) => {
+  const items = [
+    {
+      title: "E",
+      message: "JS renderiza HTML com JS",
+    },
+    {
+      title: "J",
+      message: "avascript é o futuro!",
+    },
+    {
+      title: "S",
+      message: "imples e leve",
+    },
+  ];
+  const subtitle =
+    "Uma linguagem de modelagem, para criação de página HTML usando JS";
+  response.render("pages/index", {
+    qualitys: items,
+    subtitle: subtitle,
+  });
+});
+
+app.get("/sobre", (request, response) => {
+  response.render("pages/about");
+});
+
+app.use((request, response) => {
+  response.status(404).render("pages/404");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
